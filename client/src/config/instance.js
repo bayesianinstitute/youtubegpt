e600f02d4f7778c7116bb3f340eed353d7c17c18
Afaan <afaan@bayes.global>
2024-09-09 23:40:22 +0530

    added authentication

--- client/src/config/instance.js
+++ client/src/config/instance.js
@@ -2,11 +2,16 @@
 
 import axios from 'axios';
 
+// Get token from the environment variable
+const token = process.env.REACT_APP_BEARER_TOKEN;
+
+
 // Create an Axios instance with a base URL and custom headers
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL, // Use the base URL from environment variables
   headers: {
     'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`, // Use the token from env
   },
   withCredentials: true,  // Include this if you need to send cookies or authentication headers
 });
